5ff68db4c8cfdc302cc5ea771150e22a98a1f788
TasQ <mtasdikyousuf@gmail.com>
2025-10-18 03:45:04 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.strip().lower()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a task: ")
+            todos.append(todo)
+        case "show":
+            for i, item in enumerate(todos): # enumerate function
+                    print(i + 1,". ",item,sep="")
+
+        case "edit":
+            try:
+                number = int(input("Enter a number: "))
+                number -= 1
+                new_todo = input("Enter a new task: ")
+                todos[number] = new_todo
+            except IndexError:
+                print("Invalid number — no such task exists.")
+            except ValueError:
+                print("Please enter a valid number.")
+
+        case "exit":
+            break
+        case _:
+            print("Invalid input, try again.")
+
+print("Bye!")
