1e47305b0555911a39a02c56699f473940a20d4f
TasQ <mtasdikyousuf@gmail.com>
2025-10-20 06:52:04 +0600

    introducing persistent storage via text files

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete, delete or exit: ")
     user_action = user_action.strip().lower()
 
     match user_action:
         case "add":
-            todo = input("Enter a task: ")
+            todo = input("Enter a task: ") + "\n"
+            
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+            
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+
         case "show":
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
             for i, item in enumerate(todos): # enumerate function
-                    print(i + 1,". ",item,sep="")
+                    print(i + 1,". ",item,sep="", end="")
 
         case "edit":
             try:
